32d7454108a732a716530a0ede38478d353f9580
Rob Campbell <git@raacampbell.com>
2014-04-08 08:52:17 -0400

    get rid of separate settings file. Two classes: one for the menu system and one for the LCD display handling

---
+++ menuSystem/LCDmenu.cpp
@@ -0,0 +1,390 @@
+
+/*
+  Menu.cpp - Library for building LCD menu
+*/
+
+#include "Arduino.h"
+#include "LCDmenu.h"
+
+
+//Initialise static const arrays for the special characters
+//http://stackoverflow.com/questions/11367141/how-to-declare-and-initialize-a-static-const-array-as-a-class-member
+ byte menuDisplay::_upArrow[8] = 
+  {
+    B00100,
+    B01010,
+    B10001,
+    B00100,
+    B01010,
+    B10001,
+    B00000};
+
+ byte menuDisplay::_downArrow[8] = 
+  {
+    B00000,
+    B10001,
+    B01010,
+    B00100,
+    B10001,
+    B01010,
+    B00100};
+
+
+
+
+
+static LiquidCrystal this_lcd(7, 8, 9, 10, 11, 12); //EDIT THIS LINE FOR YOUR WIRING SETTINGS
+LiquidCrystal* menuDisplay::lcd = &this_lcd;
+
+
+menuDisplay::menuDisplay(byte lcdRows, byte lcdCols, int buttonPin, int xLine, int yLine)
+{
+  _lcdRows=lcdRows;
+  _lcdCols=lcdCols;
+  _buttonPin=buttonPin;
+  _xLine=xLine;
+  _yLine=yLine;
+
+  _arrowPos=0; //arrowPos is the line at which to draw the arrow in the first column of the LCD
+  _currentTopRow=0; //This is the row of the menu currently on row zero of the LCD
+  
+  _deadZone=20; //Make sure the stick is moved a good amount before ticks are added to the counter
+  _centre=515; //Value of stick in the centre
+
+  //The following variables are used to tell for how long the 
+  //user has been pushing at the stick. This allows us to 
+  //increment numbers by 10x or 100x, as desired. 
+  _numIncrements=0; //The number of continuous increments 
+  _lastIncremented=0; //Was the variable incremented in the last pass?
+  _incrementBy=1;
+
+}
+
+void menuDisplay::initDisplay()
+{
+
+  pinMode(_buttonPin,INPUT); 
+
+  lcd->begin(_lcdCols, _lcdRows);
+  lcd->createChar(0, _upArrow);
+  lcd->createChar(1, _downArrow);  
+  lcd->setCursor(0,0);
+  refreshScreen();
+
+}
+
+void menuDisplay::changeMenu(Menu newMenu[], int nRows){
+  currentMenu=newMenu;
+  nMenuRows=nRows;
+  _currentTopRow=0;
+  _arrowPos=0;
+  refreshScreen();
+}
+
+
+
+long menuDisplay::poll(){
+ 
+  //Read the X and Y directions of the stick
+  short x=analogRead(_xLine);
+  short y=analogRead(_yLine);
+
+  short delta_y=y-_centre;  
+  short delta_x=x-_centre;  
+   
+  //How long to wait before leaving function. This number determines 
+  //how quickly digits change and the arrow progresses
+  float waitTime=0; 
+
+  //Move the little arrow indicating the line we're on. Scroll
+  //screen if appropriate. 
+  if (abs(delta_y)>_deadZone){
+   //wait and re-read to avoid reading values before thumbstick has settled
+    delay(20);
+    short y=analogRead(_yLine);
+    delta_y=y-_centre;  
+
+   if (y>_centre){
+     _arrowPos--;
+    } else if (y<_centre){
+     _arrowPos++;
+    }
+   updateCursor();
+
+   //Wait before updating
+   waitTime = _centre-abs(delta_y);
+   waitTime = 150 + (waitTime / (2.0 * _centre)) * 1000.0; //Never faster than 100ms
+  }//if (abs(delta_y)...
+
+
+  //Allow updates of the values on the current row
+  byte menuRow=_arrowPos+_currentTopRow; //The menu row (allows for scrolling)
+
+  if (abs(delta_x)>_deadZone)
+  {
+
+    if (_numIncrements>=4){
+      if ( (currentMenu[menuRow].value % (_incrementBy*10) )==0){
+        _numIncrements=1;
+        _incrementBy *= 10; 
+      }     
+    }
+
+
+    if (x>_centre)
+    {
+     currentMenu[menuRow].incrementVal(_incrementBy);
+    } else if (x<_centre){
+     currentMenu[menuRow].incrementVal(-1 * _incrementBy);
+    }
+      
+    updateRowValue(menuRow);
+      
+
+    //Apply wait
+    waitTime = _centre-abs(delta_x);
+    waitTime = 150 + (waitTime / (2.0 * _centre)) * 1000.0; //Never faster than 100ms
+     
+    if (_lastIncremented){
+     _numIncrements++;
+    }
+   
+    _lastIncremented=1;
+
+  } else {
+
+    _lastIncremented=0;
+    _numIncrements=0;
+    _incrementBy=1;
+
+  }//if (abs(delta_x)...
+
+  //Finally, check if button is pressed and, if so, run the appropriate function if this is possible
+  if (digitalRead(_buttonPin)==HIGH){
+    if (!currentMenu[menuRow].isVariable){
+      currentMenu[menuRow].buttonFunction();
+    }
+  } 
+
+  
+  if (waitTime>0){
+    long currentMillis = millis();
+    while (millis()-currentMillis < waitTime){}
+  }
+
+  return waitTime;
+
+
+}
+
+
+
+
+//re-draw the whole screen
+void menuDisplay::refreshScreen(){
+  //  int nMenuRows=3; // FIX THIS HACK!!!!
+
+ byte screenRow=0;
+ for (byte menuRow=_currentTopRow; menuRow<_currentTopRow+_lcdRows; menuRow++){
+   if (screenRow>=nMenuRows){
+     lcd->setCursor(0,screenRow++);
+    for (int ii=0; ii<_lcdCols; ii++){  lcd->print(" "); } //wipe whole line
+   } else {
+    lcd->setCursor(1,screenRow++);
+    lcd->print(currentMenu[menuRow].menuString());
+    updateRowValue(menuRow);
+   } // if (screenRow>nMenuRows)
+   
+ }
+
+
+ updateCursor(); //The highlighted line
+ 
+ //Add up or down arrows depending on where we are in the
+ //menu system
+ lcd->setCursor(_lcdCols-1,0); lcd->print(" ");
+ lcd->setCursor(_lcdCols-1,_lcdRows-1); lcd->print(" ");
+
+ if (_currentTopRow>0){
+   lcd->setCursor(_lcdCols-1,0);
+   lcd->write(byte(0));
+ }
+ if ((_currentTopRow+_lcdRows)<nMenuRows){
+   lcd->setCursor(_lcdCols-1,_lcdRows-1);
+   lcd->write(byte(1));
+ }
+
+ 
+} //void refreshScreen
+
+
+
+//update the little arrow (cursor) as the user pushes the thumbstick
+void menuDisplay::updateCursor(){
+ byte lastRow;
+
+ if (nMenuRows>_lcdRows){
+   lastRow=_lcdRows;
+ } else {
+   lastRow=nMenuRows;
+ }
+
+ //clear arrow position row
+ for (byte ii=0; ii<_lcdRows; ii++){
+  lcd->setCursor(0,ii);
+  lcd->print(" ");
+ }
+
+ //If arrow is within bounds we print it
+ if (_arrowPos>=0 && _arrowPos<lastRow){
+   printArrow();
+   return;
+ } 
+
+ //If arrow has moved beyond the lowest menu row but this isn't the lowest
+ //lcd row, then don't allow arrow beyond this point.  
+ if (_arrowPos>=lastRow && lastRow<_lcdRows){
+   _arrowPos=lastRow-1;
+   printArrow();
+   return;
+ } 
+
+
+ //If arrow has gone beyond the top row, we keep it at top row
+ if (_arrowPos < 0){
+   _arrowPos=0;
+   printArrow();
+   if (_currentTopRow>0){ //If scroll upwards if required
+      _currentTopRow--;
+      refreshScreen();
+   }
+
+   return;
+ }
+
+
+ //If arrow has gone beyond the bottom lcd row, we keep it at bottom row
+ if (_arrowPos>=_lcdRows){
+   _arrowPos=_lcdRows-1;
+   printArrow();   
+   if (nMenuRows-_lcdRows-_currentTopRow > 0){ //If there are more rows to go we scroll down
+     _currentTopRow++;
+     refreshScreen();   
+   }
+
+   return;
+ }
+
+   
+} //void updateCursor
+
+
+void menuDisplay::updateRowValue(byte menuRow){
+   
+   //Wipe the region of the row where the value will be placed
+   //byte cursorPos=currentMenu[menuRow].menuStringLength+1;
+   byte cursorPos=currentMenu[menuRow].menuStringLength();
+   byte screenRow=menuRow-_currentTopRow;
+
+
+   for (byte ii=cursorPos; ii<=_lcdCols-2; ii++){
+    lcd->setCursor(ii,screenRow); 
+    lcd->print(" ");  
+   }
+   
+   if (currentMenu[menuRow].isVariable){
+     lcd->setCursor(cursorPos,screenRow); 
+     lcd->print(currentMenu[menuRow].value);  
+   }
+
+} //void updateRowValue
+
+
+
+
+//void menuDisplay::printArrow(byte screenRow){
+void menuDisplay::printArrow(){
+   lcd->setCursor(0,_arrowPos);
+   lcd->print(">");
+}
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------
+// CLASS MENU FOLLOWS
+Menu::Menu(){
+
+}
+
+
+
+void Menu::setNumericMenu(short val,short minVal,short maxVal,bool wrap,String menuStr)
+{
+	isVariable=1;
+	value=val;
+	_menuString=menuStr;
+	_maxVal=maxVal;
+	_minVal=minVal;
+	_wrap=wrap;
+}
+
+void Menu::setActionMenu(String menuStr, void (*funct)() )
+{
+	isVariable=0;
+	_menuString=menuStr;
+	buttonFunction=funct;
+}
+
+
+/**
+Chops off the end of the menu string if it's too long given the number of characters
+taken up by the value. Displaying the value takes priority over displaying the menustring 
+*/
+String Menu::menuString(){
+  int usableCols=18; //HACK UNTIL I FIGURE OUT HOW TO GET THE CLASSES TO TALK 
+  int maxDigits=ceil(log10(_maxVal));
+
+  int maxStringLength = usableCols - maxDigits-1;
+  String tmpStr = _menuString;
+  if (tmpStr.length() > maxStringLength){
+    tmpStr = _menuString.substring(0,maxStringLength);
+    tmpStr.concat(":");
+  }
+ 
+  return tmpStr;
+}
+
+//Increment value whilst staying in bounds. 
+void Menu::incrementVal(short incrementBy){
+  //LIKELY THIS NEEDS TO BE ABLE TO TALK BACK TO THE menuDisplay CLASS IN ORDER
+  //TO: A) SET _incrementBy TO 1 WHEN IT WRAPS. B) NOT ALLOW IT TO, SAY, INCREMENT BY 100
+  //IF value IS ONLY 100 AWAY FROM IT'S LIMIT VALUE. 
+    value += incrementBy;
+
+    if (value < _minVal){
+      if (_wrap){
+        value = _maxVal;        
+      } else {
+        value = _minVal;
+      }
+    }
+
+    if (value > _maxVal){
+      if (_wrap){
+        value = _minVal;
+      } else {
+       value = _maxVal;
+      }
+    }
+}
+
+byte Menu::menuStringLength(){
+  return menuString().length()+1;
+}
+
--- menuSystem/menuSystem.ino
+++ menuSystem/menuSystem.ino
@@ -1,313 +1,92 @@
+
 /*                                                                                
- Set up a menu system using a class 
+ Set up a menu system using two classes: one for the menu system and one for 
+ the menu data
 */
 
 
 // Include the LCD library
 #include "LiquidCrystal.h"
-#include "settings.h"
-#include "Menu.h"
+#include "LCDmenu.h"
 
 
 
 //Define the settings menu
-const int settingsMenuLength=6;
+const int settingsMenuLength=7;
 Menu settingsMenu[settingsMenuLength] ;
-Menu* thisMenuPtr = settingsMenu;
-byte nMenuRows=settingsMenuLength; //Number of entries in settingsMenu
 
 //Define another menu (mainMenu) to link to as an example
 const int mainMenuLength=3;
 Menu mainMenu[mainMenuLength];
 
 
+menuDisplay myDisplay(4,20,13,0,1); //lcdRows, lcdCols, thumbStick pin, x pin, y pin
+
 
 void setup() {
+
   Serial.begin(9600);          //  setup serial
-  pinMode(buttonPin,INPUT);
 
   //Define the settings menu
   settingsMenu[0].setNumericMenu( 4, 1,  10, 1, "Taste 1 Angle:");
-  settingsMenu[1].setNumericMenu(45, 1, 180, 1, "dAngle:");
+  settingsMenu[1].setNumericMenu(45, 1, 15000, 1, "dAngle:");
   settingsMenu[2].setNumericMenu(80, 2, 178, 0, "Extended pos:");
   settingsMenu[3].setNumericMenu(70, 2, 178, 0, "Retracted pos:");
-  settingsMenu[4].setActionMenu("To main menu",toMainMenu);
-  settingsMenu[5].setActionMenu("Wipe and re-draw",wiper);
-
+  settingsMenu[4].setNumericMenu(70, 2, 178, 0, "RETRACTED pos:");
+  settingsMenu[5].setActionMenu("To main menu",toMainMenu);
+  settingsMenu[6].setActionMenu("Wipe and re-draw",wiper);
+  
   //Define the mainMenu
   mainMenu[0].setNumericMenu(4, 1,  10, 1, "Something:");
   mainMenu[1].setNumericMenu(4, 1,  10, 1, "Something ELSE:");
   mainMenu[2].setActionMenu("BACK",toSettingsMenu);
 
 
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(lcdCols, lcdRows);
-  lcd.createChar(0, upArrow);
-  lcd.createChar(1, downArrow);  
+  //Initialise display and set menu
+  myDisplay.initDisplay();
+  myDisplay.changeMenu(settingsMenu,settingsMenuLength);
 
-  //Bring up the menu
-  refreshScreen();  
-  /*
-  Serial.println("Start debug");
-  Serial.println(sizeof(thisMenuPtr));
-  Serial.println(sizeof(thisMenuPtr[0]));
-  Serial.println(sizeof(thisMenuPtr)/sizeof(thisMenuPtr[0]));
-  Serial.println("End debug");
-*/
 }
 
 
 
 void loop() {
-  
-  //The following variables are used to tell for how long the 
-  //user has been pushing at the stick. This allows us to 
-  //increment numbers by 10x or 100x, as desired. 
-  static unsigned long numIncrements=0; //The number of continuous increments 
-  static bool lastIncremented=0; //Was the variable incremented in the last pass?
-
   //Following variables are "wait-without-delay" counters for listening to the 
-  //thumbstick. By default the thumbstick code will be executed every varInterval ms. 
-  //However, this rate is modifed by the stick position. We want to check more often 
-  //when the stick is untouched (improved responsiveness) and less often whilst we're 
-  //scrolling (to maintain reasonable scroll speed). 
+  //thumbstick. 
   static long varInterval=25; 
   static unsigned long currentVarMillis;
   static long previousVarMillis=0;
 
-
-  //Read stick every varInterval ms (determined by last-read stick position)
-  //and update screen as needed. 
   currentVarMillis=millis();
   if(currentVarMillis - previousVarMillis > varInterval) {  
     previousVarMillis=currentVarMillis; //save update time
         
-
-    //Read the X and Y directions of the stick
-    short y=analogRead(A1);
-    short x=analogRead(A0);
-
-    short delta_y=y-centre;  
-    short delta_x=x-centre;  
-
-
-    //Determines the speed with which the screen updates during 
-    //user interaction 
-    if (abs(delta_y)>deadZone || abs(delta_x)>deadZone){
-       varInterval=centre-abs(x-centre)*0.6; 
-    } else {
-       varInterval=25;
-    }
-
-
-
-    //Move the little arrow indicating the line we're on. Scroll
-    //screen if appropriate. 
-    if (abs(delta_y)>deadZone){
-     if (y>centre){
-       arrowPos--;
-      } else if (y<centre){
-       arrowPos++;
-      }
-     updateCursor();
-    }//if (abs(delta_y)...
-
-
-    //Allow updates of the values on the current row
-    byte menuRow=arrowPos+currentTopRow; //The menu row (allows for scrolling)
-
-    if (abs(delta_x)>deadZone){
-      byte incrementBy; //By how much to increment   
-
-      if (numIncrements>=4 && (thisMenuPtr[menuRow].value % 10)==0)
-      {
-       incrementBy=10;
-      } else {
-       incrementBy=1;
-      }
-
-      if (x>centre)
-      {
-       thisMenuPtr[menuRow].incrementVal(incrementBy);
-      } else if (x<centre){
-       thisMenuPtr[menuRow].incrementVal(-incrementBy);
-      }
-      
-     
-     
-
-      updateRowValue(menuRow);
-      
-      
-      if (lastIncremented){
-	      numIncrements++;
-      }
-      lastIncremented=1;
-    } else {
-      lastIncremented=0;
-      numIncrements=0;
-    }//if (abs(delta_x)...
-
-
-    //Finally, check if button is pressed and, if so, run the appropriate function if this is possible
-    if (digitalRead(buttonPin)==HIGH){
-      if (!thisMenuPtr[menuRow].isVariable){
-        thisMenuPtr[menuRow].buttonFunction();
-      }
-    } 
-
+    myDisplay.poll();
   } //if(currentVarMillis ...
-  
+
+
 } //function loop
 
 
 
 
 
-//re-draw the whole screen
- 
- void refreshScreen(){
-
- byte screenRow=0;
- for (byte menuRow=currentTopRow; menuRow<currentTopRow+lcdRows; menuRow++){
-   if (screenRow>=nMenuRows){
-    lcd.setCursor(0,screenRow++);
-    for (int ii=0; ii<lcdCols; ii++){  lcd.print(" "); } //wipe whole line
-   } else {
-    lcd.setCursor(1,screenRow++);
-    lcd.print(thisMenuPtr[menuRow].menuString);
-    updateRowValue(menuRow);
-   } // if (screenRow>nMenuRows)
-   
- }
-
-
- updateCursor(); //The highlighted line
- 
- //Add up or down arrows depending on where we are in the
- //menu system
- lcd.setCursor(lcdCols-1,0); lcd.print(" ");
- lcd.setCursor(lcdCols-1,lcdRows-1); lcd.print(" ");
-
- if (currentTopRow>0){
-   lcd.setCursor(lcdCols-1,0);
-   lcd.write(byte(0));
- }
- if ((currentTopRow+lcdRows)<nMenuRows){
-   lcd.setCursor(lcdCols-1,lcdRows-1);
-   lcd.write(byte(1));
- }
- 
- 
-} //void refreshScreen
-
-
-//update the little arrow (cursor) as the user pushes the thumbstick
-void updateCursor(){
- byte lastRow;
-
- if (nMenuRows>lcdRows){
-   lastRow=lcdRows;
- } else {
-   lastRow=nMenuRows;
- }
-
- //clear arrow position row
- for (byte ii=0; ii<lcdRows; ii++){
-  lcd.setCursor(0,ii);
-  lcd.print(" ");
- }
-
- //If arrow is within bounds we print it
- if (arrowPos>=0 && arrowPos<lastRow){
-   printArrow(arrowPos);
-   return;
- } 
-
- //If arrow has moved beyond the lowest menu row but this isn't the lowest
- //lcd row, then don't allow arrow beyond this point.  
- if (arrowPos>=lastRow && lastRow<lcdRows){
-   arrowPos=lastRow-1;
-   printArrow(arrowPos);
-   return;
- } 
-
-
- //If arrow has gone beyond the top row, we keep it at top row
- if (arrowPos < 0){
-   arrowPos=0;
-   printArrow(arrowPos);
-   if (currentTopRow>0){ //If scroll upwards if required
-      currentTopRow--;
-      refreshScreen();
-   }
-
-   return;
- }
-
-
- //If arrow has gone beyond the bottom lcd row, we keep it at bottom row
- if (arrowPos>=lcdRows){
-   arrowPos=lcdRows-1;
-   printArrow(arrowPos);   
-   if (nMenuRows-lcdRows-currentTopRow > 0){ //If there are more rows to go we scroll down
-     currentTopRow++;
-     refreshScreen();   
-   }
-
-   return;
- }
-
-   
-} //void updateCursor
-
-
-void updateRowValue(byte menuRow){
-   
-   //Wipe the region of the row where the value will be placed
-   //byte cursorPos=thisMenuPtr[menuRow].menuStringLength+1;
-   byte cursorPos=thisMenuPtr[menuRow].menuStringLength();
-   byte screenRow=menuRow-currentTopRow;
-
-
-   for (byte ii=cursorPos; ii<=lcdCols-2; ii++){
-    lcd.setCursor(ii,screenRow); 
-    lcd.print(" ");  
-   }
-   
-   if (thisMenuPtr[menuRow].isVariable){
-     lcd.setCursor(cursorPos,screenRow); 
-     lcd.print(thisMenuPtr[menuRow].value);  
-   }
-
-} //void updateRow
-
-
-void printArrow(byte screenRow){
-   lcd.setCursor(0,screenRow);
-   lcd.print(">");
-}
 
 
 //TEST STUFF BELOW
 void wiper(){
-  lcd.clear();
+  myDisplay.lcd->clear();
   delay(3000);
-  refreshScreen();
+  myDisplay.refreshScreen();
 }
 
+
 void toMainMenu(){
-   nMenuRows=mainMenuLength;
-   thisMenuPtr=mainMenu; //Switch to new menu
-   currentTopRow=0;
-   arrowPos=0;
-   refreshScreen();
+   myDisplay.changeMenu(mainMenu,mainMenuLength);
+   
 }
 
 void toSettingsMenu(){
-   nMenuRows=settingsMenuLength;
-   thisMenuPtr=settingsMenu; //Switch to new menu
-   currentTopRow=0;
-   arrowPos=0;
-   refreshScreen();
+   myDisplay.changeMenu(settingsMenu,settingsMenuLength);
 }
+
