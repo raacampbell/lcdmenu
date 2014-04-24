b64466c82ac211326362cef7d0ebb337703f75fc
raacampbell <git@raacampbell.com>
2014-04-24 00:26:10 -0400

    Begin modifying code to accept input from NIDAQ to move servo and actuator.

--- tastulator/tastulator.ino
+++ tastulator/tastulator.ino
@@ -33,7 +33,7 @@ menuDisplay myDisplay(&this_lcd,lcdRows,lcdCols,tStickButton,tStickXPin,tStickYP
 
 //Declare two instances of class Menu, which will house the menus that will be displayed
 //The first class has 7 rows and the second has three rows. 
-const int settingsMenuLength=8; //The number of rows in the menu
+const int settingsMenuLength=9; //The number of rows in the menu
 Menu settingsMenu[settingsMenuLength];
 
 const int controlMenuLength=4;
@@ -44,31 +44,37 @@ Menu statusMenu[statusMenuLength]; //Define another menu to link to as an exampl
 
 
 //We will have the ability to load the values of settingsMenu from the EEPROM
-const byte lengthValues=5;
-short values[lengthValues]={80,70,3,30,4}; //Default values. 
+const byte lengthValues=6;
+short values[lengthValues]={80,70,3,30,4,0}; //Default values. 
 
 //Create pointers to menu entries we will need to access at run-time
 Menu* pZERO_ANG   = &settingsMenu[2];
 Menu* pDELTA_ANG  = &settingsMenu[3];
-Menu* pNTASTES = &settingsMenu[4];
-Menu* pSAVE = &settingsMenu[5];
-Menu* pLOAD = &settingsMenu[6];
+Menu* pNTASTES  = &settingsMenu[4];
+Menu* pNIDAQ_IN = &settingsMenu[5];
+Menu* pSAVE = &settingsMenu[6];
+Menu* pLOAD = &settingsMenu[7];
 
 
 Menu* pSELECT_TASTE = &controlMenu[0]; 
 Menu* pSERVO_ANG    = &controlMenu[1]; 
+Menu* pACTUATOR_POS = &controlMenu[2]; 
 
 Menu* pCUR_TASTE = &statusMenu[0]; 
 Menu* pCUR_ANGLE = &statusMenu[1];
-Menu* pCUR_POS   = &statusMenu[2];
+//Menu* pCUR_POS   = &statusMenu[2];
 
 
 
 //Motor pins
 const byte servoPin=3;
 const byte actuatorPin=5;
-Servo myservo;  
+Servo myServo;  
 
+//Binary inputs for control via a NIDAQ 
+const byte IN1=6; //Binary input 1
+const byte IN2=4; //Binary input 2
+const byte ACT=2; //Extend/Retract 
 
 //These will need to be dealt with
 short currentTaste=1;
@@ -79,7 +85,15 @@ short currentPos=40;
 void setup() {
 
   Serial.begin(9600);
-  myservo.attach(servoPin); 
+  myServo.attach(servoPin); 
+  pinMode(actuatorPin,OUTPUT);
+
+  //Set up binary inputs we will use to read from NIDAQ
+  //[do we need pullup/pulldown?]
+  pinMode(IN1,INPUT);
+  pinMode(IN2,INPUT);
+  pinMode(ACT,INPUT);
+
 
   //Define the settings menu, which is an array of class Menu. The order 
   //of the input arguments is: 
@@ -90,12 +104,13 @@ void setup() {
   // 5. name of row printed to LCD display [String]
   settingsMenu[0].setNumericMenu(values[0], 2, 178, 0, "Extended pos:");
   settingsMenu[1].setNumericMenu(values[1], 2, 178, 0, "Retracted pos:");
-  settingsMenu[2].setNumericMenu(values[2],  1,  90, 0, "Taste zero ang:");
+  settingsMenu[2].setNumericMenu(values[2],  1, 90, 0, "Taste zero ang:");
   settingsMenu[3].setNumericMenu(values[3], 5,  45, 1, "dAngle:");
   settingsMenu[4].setNumericMenu(values[4], 1,  10, 1, "#Tastes:");
-  settingsMenu[5].setActionMenu("SAVE",save);
-  settingsMenu[6].setActionMenu("LOAD",load);
-  settingsMenu[7].setActionMenu("BACK",toStatusMenu); //executes toMainMenu() when button is pressed
+  settingsMenu[5].setNumericMenu(values[5], 0,   1, 0, "NIDAQ in [0/1]:");
+  settingsMenu[6].setActionMenu("SAVE",save);
+  settingsMenu[7].setActionMenu("LOAD",load);
+  settingsMenu[8].setActionMenu("BACK",toStatusMenu); //executes toMainMenu() when button is pressed
 
   
   //Define the control menu
@@ -139,22 +154,25 @@ void loop() {
     }
     //Update the status menu
     updateFixedMenu(pCUR_TASTE,pSELECT_TASTE->value);
-    updateFixedMenu(pCUR_POS,pSERVO_ANG->value);
+    updateFixedMenu(pCUR_ANGLE,pSERVO_ANG->value);
+
+     
   } //if (currentVarMillis 
 
 
   //If the user modifies the control settings for current taste then we need to act on this
   //Some settings potentially conflict (e.g. select taste and servo angle) so we want the
   //most recently modified number to take precedence and to modify the other number. 
-  static long controlTaste = pSELECT_TASTE->value;
-  static long controlAngle = pSERVO_ANG->value;
+  static short controlTaste = pSELECT_TASTE->value;
+  static short controlAngle = pSERVO_ANG->value;
+  static short actuatorPos  = pACTUATOR_POS->value;
 
   if (controlTaste != pSELECT_TASTE->value){
     pSERVO_ANG->setValue(taste2angle(pSELECT_TASTE->value));
     controlTaste=pSELECT_TASTE->value;
     controlAngle = pSERVO_ANG->value;
     myDisplay.refreshScreen();
-    myservo.write(pSERVO_ANG->value); 
+    myServo.write(pSERVO_ANG->value); 
   }
 
  if (controlAngle != pSERVO_ANG->value){
@@ -162,9 +180,13 @@ void loop() {
     controlAngle = pSERVO_ANG->value;
     controlTaste = pSELECT_TASTE->value;
     myDisplay.refreshScreen();
-    myservo.write(pSERVO_ANG->value); 
+    myServo.write(pSERVO_ANG->value); 
   }
 
+ if (actuatorPos != pACTUATOR_POS->value){
+  analogWrite(actuatorPin,actuatorPos);
+  actuatorPos = pACTUATOR_POS->value;
+ }
 } //function loop
 
 
@@ -218,7 +240,7 @@ void load(){
  // void eeprom_read_block(void *  __dst, const void * __src, size_t __n )
  // Read a block of __n bytes from EEPROM address __src to SRAM __dst.
  eeprom_read_block((void*)&values, (void*)0, sizeof(values));
-  for (byte ii=0; ii<5; ii++){
+  for (byte ii=0; ii<lengthValues; ii++){
     settingsMenu[ii].setValue(values[ii]);
   }
   
@@ -258,11 +280,15 @@ void updateSAVE(){
 
 
 //Functions to convert back and forth between tastes and angles
-long angle2taste(long angle){
-  return round((angle-pZERO_ANG->value)/pDELTA_ANG->value);
+short angle2taste(short angle){
+  short out = round((angle-pZERO_ANG->value)/pDELTA_ANG->value)+1;
+  if (out>pNTASTES->value){
+    out=pNTASTES->value;
+  }
+  return out;
 }
 
-long taste2angle(long taste){
+short taste2angle(short taste){
   return (taste*pDELTA_ANG->value)+pZERO_ANG->value;
 
 }
